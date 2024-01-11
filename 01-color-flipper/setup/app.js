057ab97a90a16fee8d388818f6343bc04ceeb761
DemetriG <demetri@nimbushosting.co.uk>
2024-01-11 09:58:24 +0000

    Project 1

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+//JS BELOW
+btn.addEventListener('click', function(){
+//     get random number between 0 -3
+    const randomNumber = getRandomNumber();
+    // set the colour
+    document.body.style.backgroundColor = colors[randomNumber];
+     // display the colour name
+     color.textContent = colors[randomNumber];
+
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
